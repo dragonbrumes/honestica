ec9b530b31910be91c4e1a9ab451465e061a260e
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2019-04-01 10:30:31 +0200

    add app in Electron

--- src/Components/Dropzone.jsx
+++ src/Components/Dropzone.jsx
@@ -5,6 +5,19 @@ import Dropzone from "react-dropzone";
 
 import "bulma/css/bulma.css";
 
+const getColor = props => {
+  if (props.isDragAccept) {
+    return "#00e676";
+  }
+  if (props.isDragReject) {
+    return "#ff1744";
+  }
+  if (props.isDragActive) {
+    return "#6c6";
+  }
+  return "#666";
+};
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -22,7 +35,9 @@ const DropContainer = styled.div`
   width: 90%;
   height: 200px;
   margin: 1em auto;
-  border: 1px dashed black;
+  border-width: 2px;
+  border-style: ${props => (props.isDragActive ? "solid" : "dashed")};
+  border-color: ${props => getColor(props)};
 `;
 
 const initialState = {
@@ -33,7 +48,6 @@ const initialState = {
   upLoadStatusText: undefined,
   upLoadStatus: null,
   upLoadReturnDataID: null
-  // binaryTotal: null
 };
 
 export default class DropZone extends Component {
@@ -48,7 +62,7 @@ export default class DropZone extends Component {
   };
 
   // send upload & fetch total of binaries  files
-  onSend = () => {
+  onSend = async () => {
     // is uploading indicator
     this.setState({ isUploading: true });
 
@@ -56,7 +70,7 @@ export default class DropZone extends Component {
     const { dropFile } = this.state;
     // sending file to api
     try {
-      axios({
+      await axios({
         method: "post",
         url: "https://fhirtest.uhn.ca/baseDstu3/Binary",
         data: dropFile
@@ -85,21 +99,25 @@ export default class DropZone extends Component {
   };
 
   onDrop = acceptedFiles => {
-    console.log(acceptedFiles);
-    this.setState({
-      dropFile: acceptedFiles,
-      dropFileName: acceptedFiles[0].name,
-      dropActive: true
-    });
+    if (acceptedFiles.length > 0) {
+      // console.log(acceptedFiles);
+      this.setState({
+        dropFile: acceptedFiles,
+        dropFileName: acceptedFiles[0].name,
+        dropActive: true
+      });
+    }
   };
 
   render() {
+    const maxSize = 2097152;
     const {
       dropActive,
       dropFileName,
       upLoadStatus,
       isUploading,
-      binaryTotal
+      binaryTotal,
+      upLoadReturnDataID
     } = this.state;
 
     return (
@@ -108,94 +126,125 @@ export default class DropZone extends Component {
           You can upload here a report. Please be award that your report is
           shared with practitioners.
         </h2>
-        <Dropzone onDrop={this.onDrop}>
-          {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
-            <DropContainer {...getRootProps()}>
-              {/* Reactive Drop zone */}
-              {!dropActive && !upLoadStatus && (
-                <div>
+        <Dropzone
+          onDrop={this.onDrop}
+          accept="application/pdf"
+          maxSize={maxSize}
+        >
+          {({
+            getRootProps,
+            getInputProps,
+            isDragActive,
+            isDragAccept,
+            isDragReject,
+            rejectedFiles
+          }) => {
+            const isFileTooLarge =
+              rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+
+            return (
+              <DropContainer
+                {...getRootProps({
+                  isDragActive,
+                  isDragAccept,
+                  isDragReject
+                })}
+              >
+                {/* Reactive Drop zone */}
+                {!dropActive && !upLoadStatus && (
                   <div>
-                    <i
-                      className="far fa-plus-square fa-3x"
-                      style={{ color: "#057550" }}
-                    />
-                  </div>
-                  <div>
-                    <input {...getInputProps()} />
-                  </div>
-                  <div
-                    style={{
-                      fontWeight: "bold",
-                      fontSize: "1.5rem"
-                    }}
-                  >
-                    {!isDragActive && "Click here or drop a file."}
-                    {isDragActive && !isDragReject && "Drop it to select!"}
-                    {isDragReject &&
-                      "This file type is not accepted! Try with .pdf"}
-                  </div>
-                  <div>
-                    {/* eslint-disable-next-line */}
-                    <a className="button is-link" style={{ width: "90%" }}>
-                      Select file to upload
-                    </a>
-                  </div>
-                </div>
-              )}
-              {/* Drop zone with element on standby */}
-              {dropActive && !upLoadStatus && !isUploading && (
-                <div>
-                  <div>
-                    Your file <strong>{dropFileName}</strong> is ready to be
-                    uploaded
-                  </div>
-                  <div>
-                    {/* eslint-disable-next-line */}
-                    <a
-                      className="button is-success"
+                    <div>
+                      <i
+                        className="far fa-plus-square fa-3x"
+                        style={{ color: "#057550" }}
+                      />
+                    </div>
+                    <div>
+                      <input {...getInputProps()} />
+                    </div>
+                    <div
                       style={{
-                        width: "90%",
-                        marginTop: "1em",
-                        marginBottom: "1em"
+                        fontWeight: "bold",
+                        fontSize: "1.5rem"
                       }}
-                      onClick={this.onSend}
                     >
-                      Upload
-                    </a>
-                    {/* eslint-disable-next-line */}
-                    <a
-                      className="button is-danger"
-                      style={{ width: "90%" }}
-                      onClick={() =>
-                        this.setState({
-                          ...initialState,
-                          binaryTotal
-                        })
-                      }
-                    >
-                      Cancel
-                    </a>
+                      {!isDragActive && "Click here or drop a file."}
+                      {isDragActive && !isDragReject && "Drop it to select!"}
+                      {isDragReject && (
+                        <div className="has-text-danger">
+                          This file type is not accepted! Try with .pdf!
+                        </div>
+                      )}
+                      {isFileTooLarge && (
+                        <div className="has-text-danger">
+                          File is too large. No more than 2MO!
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      {/* eslint-disable-next-line */}
+                      <a className="button is-link" style={{ width: "250px" }}>
+                        Select file to upload
+                      </a>
+                    </div>
                   </div>
-                </div>
-              )}
-              {/* loader during file upload */}
-              {isUploading && (
-                <progress className="progress is-medium is-primary" max="100">
-                  45%
-                </progress>
-              )}
-              {/* uploading attempt file result */}
-              {upLoadStatus === 201 && (
-                <div className="notification is-success">
-                  <button
-                    className="delete"
-                    onClick={() => this.setState({ ...initialState })}
-                  />
-                  Your file <strong>{dropFileName}</strong> have been uploaded!
-                </div>
-              )}
-            </DropContainer>
-          )}
+                )}
+                {/* Drop zone with element on standby */}
+                {dropActive && !upLoadStatus && !isUploading && (
+                  <div>
+                    <div>
+                      Your file <strong>{dropFileName}</strong> is ready to be
+                      uploaded
+                    </div>
+                    <div>
+                      {/* eslint-disable-next-line */}
+                      <a
+                        className="button is-success"
+                        style={{
+                          width: "90%",
+                          marginTop: "1em",
+                          marginBottom: "1em"
+                        }}
+                        onClick={this.onSend}
+                      >
+                        Upload
+                      </a>
+                      {/* eslint-disable-next-line */}
+                      <a
+                        className="button is-danger"
+                        style={{ width: "90%" }}
+                        onClick={() =>
+                          this.setState({
+                            ...initialState,
+                            binaryTotal
+                          })
+                        }
+                      >
+                        Cancel
+                      </a>
+                    </div>
+                  </div>
+                )}
+                {/* loader during file upload */}
+                {isUploading && (
+                  <progress className="progress is-medium is-primary" max="100">
+                    45%
+                  </progress>
+                )}
+                {/* uploading attempt file result */}
+                {upLoadStatus === 201 && (
+                  <div className="notification is-success">
+                    <button
+                      className="delete"
+                      onClick={() => this.setState({ ...initialState })}
+                    />
+                    Your file <strong>{dropFileName}</strong> with id
+                    <strong>{upLoadReturnDataID}</strong> have been uploaded!
+                  </div>
+                )}
+              </DropContainer>
+            );
+          }}
         </Dropzone>
         <div
           className="tags has-addons"
